9ee03671251bbff17053b678565dbeb57e1c4b05
HrishikShaji <hrishikminishaji@gmail.com>
2023-12-25 20:12:50 +0530

    InputColors are rendered but very slow

--- app/components/InputColor.tsx
+++ app/components/InputColor.tsx
@@ -1,44 +1,66 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export type Color = {
   hue: number;
   saturation: number;
   light: number;
 };
 
-export const InputColor = () => {
-  const grids = () => {
-    const cols = 100;
-    let grids: Color[][] = [];
-    for (let r = 0; r < cols; r++) {
-      grids[r] = [];
-      for (let c = 0; c < cols; c++) {
-        grids[r][c] = {
-          hue: c * (100 / cols),
-          saturation: c * (100 / cols),
-          light: c * (100 / cols),
-        };
-      }
+const getGrids = () => {
+  const cols = 10;
+  let grids: Color[][] = [];
+  for (let r = 0; r < cols; r++) {
+    grids[r] = [];
+    for (let c = 0; c < cols; c++) {
+      grids[r][c] = {
+        hue: c * (100 / cols),
+        saturation: c * (100 / cols),
+        light: c * (100 / cols),
+      };
     }
-    return (
-      <div className="flex ">
-        {grids.map((grid, i) => (
-          <div key={i} className="flex flex-col">
-            {grid.map((item, k) => {
-              console.log(item);
-              return (
-                <div
-                  style={{
-                    backgroundColor: `hsl(${item.hue},${item.saturation}%,${item.light}%)`,
-                  }}
-                  key={k}
-                  className="w-[1px] h-[1px] bg-white text-center"
-                ></div>
-              );
-            })}
-          </div>
-        ))}
-      </div>
-    );
-  };
+  }
+  return grids;
+};
 
-  return <div>{grids()}</div>;
+export const InputColor = () => {
+  const [grids, setGrids] = useState<Color[][]>([]);
+  const [range, setRange] = useState("");
+  useEffect(() => {
+    setGrids(getGrids());
+  }, [range]);
+  return (
+    <div>
+      <div className="">
+        <div className="flex ">
+          {grids.map((grid, i) => (
+            <div key={i} className="flex flex-col">
+              {grid.map((item, k) => {
+                console.log(item);
+                return (
+                  <div
+                    style={{
+                      backgroundColor: `hsl(${parseInt(range)},${
+                        item.saturation
+                      }%,${item.light}%) `,
+                    }}
+                    key={k}
+                    className="w-10 h-10 bg-white text-center"
+                  ></div>
+                );
+              })}
+            </div>
+          ))}
+        </div>
+        <input
+          className="w-full"
+          type="range"
+          min="0"
+          max="360"
+          onChange={(e) => setRange(e.target.value)}
+        />
+      </div>
+    </div>
+  );
 };
