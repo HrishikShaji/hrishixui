7406115625ee61435c871511ce14e16f5f469a37
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-04 22:16:58 +0530

    removed useGridsLogic

--- app/components/InputColor.tsx
+++ app/components/InputColor.tsx
@@ -1,5 +1,25 @@
 "use client";
 
+import { useState } from "react";
+import { ColorPicker } from "./ColorPicker";
+
 export const InputColor = () => {
-  return <div className="relative"></div>;
+	const [selectedColor, setSelectedColor] = useState("hsl(0,0%,0%)");
+	const [isOpen, setIsOpen] = useState(false);
+
+	return (
+		<div className="relative">
+			<div
+				onClick={() => setIsOpen(!isOpen)}
+				className="w-10 h-5"
+				style={{ backgroundColor: selectedColor }}
+			/>
+			{isOpen ? (
+				<ColorPicker
+					selectedColor={selectedColor}
+					setSelectedColor={setSelectedColor}
+				/>
+			) : null}
+		</div>
+	);
 };
