6086512c9fead3163a73a00299e11116b687a8f0
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-04 22:20:50 +0530

    inputcolor stable

--- app/components/InputColor.tsx
+++ app/components/InputColor.tsx
@@ -4,22 +4,24 @@ import { useState } from "react";
 import { ColorPicker } from "./ColorPicker";
 
 export const InputColor = () => {
-	const [selectedColor, setSelectedColor] = useState("hsl(0,0%,0%)");
-	const [isOpen, setIsOpen] = useState(false);
+  const [selectedColor, setSelectedColor] = useState("hsl(0,0%,0%)");
+  const [isOpen, setIsOpen] = useState(false);
 
-	return (
-		<div className="relative">
-			<div
-				onClick={() => setIsOpen(!isOpen)}
-				className="w-10 h-5"
-				style={{ backgroundColor: selectedColor }}
-			/>
-			{isOpen ? (
-				<ColorPicker
-					selectedColor={selectedColor}
-					setSelectedColor={setSelectedColor}
-				/>
-			) : null}
-		</div>
-	);
+  return (
+    <div className="relative">
+      <div
+        onClick={() => setIsOpen(!isOpen)}
+        className="w-10 h-5"
+        style={{ backgroundColor: selectedColor }}
+      />
+      {isOpen ? (
+        <div className="absolute mt-2">
+          <ColorPicker
+            selectedColor={selectedColor}
+            setSelectedColor={setSelectedColor}
+          />
+        </div>
+      ) : null}
+    </div>
+  );
 };
