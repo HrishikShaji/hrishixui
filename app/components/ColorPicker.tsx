07cfdd58e3a78e4b611e2c17bf4b470c76fc9a3b
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-04 21:46:12 +0530

    added color palette

--- app/components/ColorPicker.tsx
+++ app/components/ColorPicker.tsx
@@ -1,186 +1,144 @@
 "use client";
 
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
-import { useGridsLogic } from "./hooks/form";
-import { Counter } from "./Counter";
+import { Dispatch, SetStateAction, useState } from "react";
 
 export type Color = {
-  hue: number;
-  saturation: number;
-  light: number;
-};
-
-const initialColor: Color = {
-  hue: 180,
-  saturation: 50,
-  light: 50,
+	hue: number;
+	sat: number;
+	lum: number;
 };
 
 export const ColorPicker = () => {
-  const [currentColor, setCurrentColor] = useState<Color>(initialColor);
-  const [isOpen, setIsOpen] = useState(false);
+	const [hue, setHue] = useState(0);
+	const [sat, setSat] = useState(0);
+	const [lum, setLum] = useState(0);
+	const [selectedColor, setSelectedColor] = useState("hsl(0,0%,0%)");
 
-  return (
-    <div className="relative">
-      <input type="color" />
-      <div
-        onClick={() => setIsOpen(!isOpen)}
-        className="h-10 flex rounded-md"
-        style={{
-          backgroundColor: `hsl(${currentColor.hue},${currentColor.saturation}%,${currentColor.light}%) `,
-        }}
-      >
-        {currentColor.hue}
-      </div>
-      {isOpen ? (
-        <div className="flex absolute mt-2 flex-col p-1 bg-white rounded-md">
-          <GenerateGrids
-            cols={5}
-            currentColor={currentColor}
-            onClick={(color: Color) => setCurrentColor(color)}
-          />
-          <div className="">
-            <SliderInput
-              title="hue"
-              name="hue"
-              min="0"
-              max="360"
-              value={currentColor}
-              setterFunction={setCurrentColor}
-            />
-          </div>
-          <div className="">
-            <SliderInput
-              title="saturation"
-              name="saturation"
-              min="0"
-              max="100"
-              value={currentColor}
-              setterFunction={setCurrentColor}
-            />
-          </div>
-          <div className="">
-            <SliderInput
-              title="light"
-              name="light"
-              min="0"
-              max="100"
-              value={currentColor}
-              setterFunction={setCurrentColor}
-            />
-          </div>
-        </div>
-      ) : null}
-    </div>
-  );
+	return (
+		<div className="p-5 bg-neutral-700 flex flex-col gap-3 rounded-md">
+			<div className="w-full h-5" style={{ backgroundColor: selectedColor }} />
+			<GenerateGrids
+				selectedColor={selectedColor}
+				cols={15}
+				setSelectedColor={setSelectedColor}
+				hue={hue}
+				sat={sat}
+				lum={lum}
+			/>
+			<Slider
+				hue={hue}
+				sat={sat}
+				lum={lum}
+				setHue={setHue}
+				setLum={setLum}
+				setSat={setSat}
+			/>
+		</div>
+	);
 };
-const GenerateGrids = (props: {
-  cols: number;
-  currentColor: Color;
-  onClick: (color: Color) => void;
+
+interface GenerateGridsProps {
+	hue: number;
+	sat: number;
+	lum: number;
+	setSelectedColor: Dispatch<SetStateAction<string>>;
+	cols: number;
+	selectedColor: string;
+}
+
+const GenerateGrids: React.FC<GenerateGridsProps> = ({
+	hue,
+	sat,
+	lum,
+	cols,
+	selectedColor,
+	setSelectedColor,
 }) => {
-  const grids = useGridsLogic({
-    cols: props.cols,
-    saturation: props.currentColor.saturation,
-    light: props.currentColor.light,
-  });
-
-  return (
-    <div className="flex ">
-      {grids.map((grid, i) => (
-        <div key={i} className="flex flex-col">
-          {grid.map((item, k) => (
-            <Grid
-              currentColor={props.currentColor}
-              key={k}
-              item={item}
-              onClick={props.onClick}
-            />
-          ))}
-        </div>
-      ))}
-    </div>
-  );
+	let grids: Color[][] = [];
+	for (let i = 0; i < cols; i++) {
+		grids[i] = [];
+		for (let j = 0; j < cols; j++) {
+			grids[i][j] = {
+				hue: hue,
+				sat: sat + j * ((100 - sat) / cols),
+				lum: lum + i * ((100 - lum) / cols),
+			};
+		}
+	}
+	return (
+		<div className="flex gap-1">
+			{grids.map((grid, i) => (
+				<div key={i} className="flex flex-col gap-1">
+					{grid.map((item, j) => {
+						const currentColor = `hsl(${item.hue},${item.sat}%,${item.lum}%)`;
+						const isSelected = selectedColor === currentColor ? true : false;
+						return (
+							<div
+								onClick={() => setSelectedColor(currentColor)}
+								className="w-4 h-4 border-2"
+								key={j}
+								style={{
+									borderColor: isSelected ? "black" : currentColor,
+									backgroundColor: currentColor,
+								}}
+							></div>
+						);
+					})}
+				</div>
+			))}
+		</div>
+	);
 };
 
-interface SliderInputProps {
-  title: string;
-  setterFunction: Dispatch<SetStateAction<Color>>;
-  min: string;
-  max: string;
-  value: Record<string, any>;
-  name: string;
+interface SliderProps {
+	setHue: Dispatch<SetStateAction<number>>;
+	setSat: Dispatch<SetStateAction<number>>;
+	setLum: Dispatch<SetStateAction<number>>;
+	hue: number;
+	sat: number;
+	lum: number;
 }
 
-const SliderInput: React.FC<SliderInputProps> = (props) => {
-  return (
-    <div className="flex flex-col  ">
-      <h1>{props.title}</h1>
-      <div className="flex gap-2">
-        <input
-          className="w-full"
-          type="range"
-          min={props.min}
-          max={props.max}
-          onChange={(e) =>
-            props.setterFunction((prev) => ({
-              ...prev,
-              [props.name]: parseInt(e.target.value),
-            }))
-          }
-          value={props.value[props.name]}
-        />
-        <input
-          min={props.min}
-          max={props.max}
-          type="number"
-          value={props.value[props.name]}
-          onChange={(e) =>
-            props.setterFunction((prev) => ({
-              ...prev,
-              [props.name]: parseInt(e.target.value),
-            }))
-          }
-          className="w-16 focus:outline-none bg-gray-500 p-1 rounded-md"
-        />
-        <Counter
-          min={parseInt(props.min)}
-          max={parseInt(props.max)}
-          value={props.value[props.name]}
-          onChange={(value) =>
-            props.setterFunction((prev) => ({
-              ...prev,
-              [props.name]: value,
-            }))
-          }
-        />
-      </div>
-    </div>
-  );
-};
-
-interface GridProps {
-  item: Color;
-  onClick: (color: Color) => void;
-  currentColor: Color;
-}
-
-const Grid: React.FC<GridProps> = (props) => {
-  console.log("each grid", props.item, props.currentColor);
-  const isCurrentColor =
-    props.item.light === props.currentColor.light &&
-    props.item.saturation === props.currentColor.saturation;
-
-  return (
-    <div
-      onClick={() => {
-        console.log(props.item, props.currentColor);
-      }}
-      style={{
-        borderColor: isCurrentColor ? "black" : "",
-        backgroundColor: `hsl(${props.currentColor.hue},${props.item.saturation}%,${props.item.light}%) `,
-      }}
-      className="w-5 h-5 cursor-pointer border-2"
-    />
-  );
+const Slider: React.FC<SliderProps> = ({
+	setHue,
+	setSat,
+	setLum,
+	hue,
+	sat,
+	lum,
+}) => {
+	return (
+		<div className="flex flex-col gap-2 text-black">
+			<div className="flex flex-col gap-1">
+				<h1>Hue</h1>
+				<input
+					value={hue}
+					type="range"
+					min="0"
+					max="360"
+					onChange={(e) => setHue(parseInt(e.target.value))}
+				/>
+			</div>
+			<div className="flex flex-col gap-1">
+				<h1>Saturation</h1>
+				<input
+					value={sat}
+					type="range"
+					min="0"
+					max="100"
+					onChange={(e) => setSat(parseInt(e.target.value))}
+				/>
+			</div>
+			<div className="flex flex-col gap-1">
+				<h1>Lumination</h1>
+				<input
+					value={lum}
+					type="range"
+					min="0"
+					max="100"
+					onChange={(e) => setLum(parseInt(e.target.value))}
+				/>
+			</div>
+		</div>
+	);
 };
