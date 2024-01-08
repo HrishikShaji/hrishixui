bd04fa2835af8256f7585104780f554fcd6af707
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-08 20:18:13 +0530

    ui changed a bit

--- app/components/InputPhone.tsx
+++ app/components/InputPhone.tsx
@@ -28,33 +28,35 @@ export const InputPhone = () => {
     <div className="flex relative flex-col gap-2">
       <input onClick={() => setIsOpen(!isOpen)} />
       {isOpen ? (
-        <div
-          ref={containerRef}
-          className="h-[500px] w-[300px] top-10 absolute flex flex-col overflow-y-scroll"
-        >
+        <div className="absolute top-10">
           <input
             type="text"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
             placeholder="Search..."
           />
-          {countryCodes.map((item) => (
-            <div
-              key={item.name}
-              className="country-item flex items-center gap-2 text-sm bg-white p-1 rounded-md"
-              data-name={item.name}
-            >
-              <Image
-                src={item.image as string}
-                alt="image"
-                height={1000}
-                width={1000}
-                className="w-6 h-6"
-              />
-              <h1 className=" inline-flex">{item.name}</h1>
-              <h1>{item?.phone ? item.phone[0] : "none"}</h1>
-            </div>
-          ))}
+          <div
+            ref={containerRef}
+            className="h-[500px] w-[300px]  flex flex-col overflow-y-scroll"
+          >
+            {countryCodes.map((item) => (
+              <div
+                key={item.name}
+                className="country-item flex items-center gap-2 text-sm bg-white p-1 rounded-md"
+                data-name={item.name}
+              >
+                <Image
+                  src={item.image as string}
+                  alt="image"
+                  height={1000}
+                  width={1000}
+                  className="w-6 h-6"
+                />
+                <h1 className=" inline-flex">{item.name}</h1>
+                <h1>{item?.phone ? item.phone[0] : "none"}</h1>
+              </div>
+            ))}
+          </div>
         </div>
       ) : null}
     </div>
