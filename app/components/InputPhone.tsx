f94a60dc409b1d3b33aa0a81c5319322d82225ce
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-08 18:10:44 +0530

    removed null data

--- app/components/InputPhone.tsx
+++ app/components/InputPhone.tsx
@@ -1,16 +1,28 @@
 import countryCodes from "@/app/country-codes.json";
+import Image from "next/image";
 
 export const InputPhone = () => {
-	return (
-		<div>
-			<input />
-			<div>
-				{countryCodes.map((item) => (
-					<div key={item.name}>
-						<h1>{item.name}</h1>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <input />
+      <div>
+        {countryCodes.map((item) => (
+          <div
+            key={item.name}
+            className="flex w-[400px] items-center gap-2 text-sm bg-white p-1 rounded-md"
+          >
+            <Image
+              src={item.image as string}
+              alt="image"
+              height={1000}
+              width={1000}
+              className="w-6 h-6"
+            />
+            <h1 className=" inline-flex">{item.name}</h1>
+            <h1>{item?.phone ? item.phone[0] : "none"}</h1>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
