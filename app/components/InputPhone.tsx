2a070017afa9d0bf46dbc3cfaa287b6fb6e38ec8
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-08 23:21:26 +0530

    error added to InputPhone

--- app/components/InputPhone.tsx
+++ app/components/InputPhone.tsx
@@ -6,6 +6,7 @@ type PhoneItem = {
 	name: string;
 	image: string;
 	phone: string;
+	phoneLength: number;
 };
 
 const initialItem: PhoneItem = {
@@ -13,12 +14,15 @@ const initialItem: PhoneItem = {
 	phone: "+91",
 	image:
 		"https://cdn.jsdelivr.net/npm/country-flag-emoji-json@2.0.0/dist/images/IN.svg",
+	phoneLength: 10,
 };
 
 export const InputPhone = () => {
 	const [isOpen, setIsOpen] = useState(false);
 	const [search, setSearch] = useState("");
 	const [selectedItem, setSelectedItem] = useState<PhoneItem>(initialItem);
+	const [error, setError] = useState<string | null>(null);
+	const [phoneNumber, setPhoneNumber] = useState("");
 
 	const containerRef = useRef<HTMLDivElement | null>(null);
 
@@ -38,10 +42,25 @@ export const InputPhone = () => {
 			}
 		}
 	}, [search]);
+
+	useEffect(() => {
+		if (phoneNumber.length !== selectedItem.phoneLength) {
+			setError("Invalid phone number");
+		} else {
+			setError(null);
+		}
+	}, [phoneNumber]);
+
 	return (
-		<div className="flex relative flex-col gap-2">
-			<div className="relative">
-				<div className="absolute left-0 z-1 flex gap-1">
+		<div className="flex relative flex-col gap-2 w-full">
+			<div className="bg-white rounded-md overflow-hidden w-full items-center flex gap-2">
+				<div
+					onClick={(e) => {
+						e.stopPropagation();
+						setIsOpen(!isOpen);
+					}}
+					className=" flex gap-1 border-r-2 border-black bg-gray-300 w-full"
+				>
 					<Image
 						src={selectedItem.image as string}
 						alt="image"
@@ -49,10 +68,15 @@ export const InputPhone = () => {
 						width={1000}
 						className="w-6 h-6"
 					/>
-					<h1>{selectedItem.phone}</h1>
+					<h1 className="inline">{selectedItem.phone}</h1>
 				</div>
-				<input onClick={() => setIsOpen(!isOpen)} />
+				<input
+					className="focus:outline-none p-1 bg-red-500"
+					value={phoneNumber}
+					onChange={(e) => setPhoneNumber(e.target.value)}
+				/>
 			</div>
+			{error ? <h1 className="text-red-500 text-sm">{error}</h1> : null}
 			{isOpen ? (
 				<div className="absolute top-10 rounded-md w-[300px] bg-white overflow-hidden">
 					<input
@@ -73,6 +97,7 @@ export const InputPhone = () => {
 										name: item.name,
 										phone: item.phone[0],
 										image: item.image,
+										phoneLength: item.phoneLength as number,
 									})
 								}
 								key={item.name}
