206c674fa48dc52380044241d6148f5007cea69c
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-09 12:57:44 +0530

    reset added to inputPhone

--- app/components/InputPhone.tsx
+++ app/components/InputPhone.tsx
@@ -2,27 +2,29 @@ import countryCodes from "@/app/country-codes.json";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 
-type PhoneItem = {
+export type PhoneItem = {
 	name: string;
 	image: string;
 	phone: string;
 	phoneLength: number;
 };
 
-const initialItem: PhoneItem = {
-	name: "India",
-	phone: "+91",
-	image:
-		"https://cdn.jsdelivr.net/npm/country-flag-emoji-json@2.0.0/dist/images/IN.svg",
-	phoneLength: 10,
-};
+interface InputPhoneProps {
+	phoneNumber: string;
+	setPhoneNumber: (value: string) => void;
+	country: PhoneItem;
+	setCountry: (value: PhoneItem) => void;
+}
 
-export const InputPhone = () => {
+export const InputPhone: React.FC<InputPhoneProps> = ({
+	phoneNumber,
+	setPhoneNumber,
+	country,
+	setCountry,
+}) => {
 	const [isOpen, setIsOpen] = useState(false);
 	const [search, setSearch] = useState("");
-	const [selectedItem, setSelectedItem] = useState<PhoneItem>(initialItem);
 	const [error, setError] = useState<string | null>(null);
-	const [phoneNumber, setPhoneNumber] = useState("");
 
 	const containerRef = useRef<HTMLDivElement | null>(null);
 
@@ -44,7 +46,7 @@ export const InputPhone = () => {
 	}, [search]);
 
 	useEffect(() => {
-		if (phoneNumber.length !== selectedItem.phoneLength) {
+		if (phoneNumber.length !== country.phoneLength) {
 			setError("Invalid phone number");
 		} else {
 			setError(null);
@@ -62,13 +64,13 @@ export const InputPhone = () => {
 					className=" flex gap-1 border-r-2 border-black bg-gray-300 w-full"
 				>
 					<Image
-						src={selectedItem.image as string}
+						src={country.image as string}
 						alt="image"
 						height={1000}
 						width={1000}
 						className="w-6 h-6"
 					/>
-					<h1 className="inline">{selectedItem.phone}</h1>
+					<h1 className="inline">{country.phone}</h1>
 				</div>
 				<input
 					className="focus:outline-none p-1 bg-red-500"
@@ -93,7 +95,7 @@ export const InputPhone = () => {
 						{countryCodes.map((item) => (
 							<div
 								onClick={() =>
-									setSelectedItem({
+									setCountry({
 										name: item.name,
 										phone: item.phone[0],
 										image: item.image,
