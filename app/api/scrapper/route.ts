e1969dc900977b870c286a8354de97b0d98f7caa
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-09 23:17:23 +0530

    JSdom added

--- app/api/scrapper/route.ts
+++ app/api/scrapper/route.ts
@@ -1,3 +1,5 @@
+import { JSDOM } from "jsdom";
+
 export async function GET(request: Request) {
 	try {
 		const { searchParams } = new URL(request.url);
@@ -10,8 +12,11 @@ export async function GET(request: Request) {
 		}
 		const data = await fetch(url);
 		const html = await data.text();
-		console.log(html);
-		return new Response(JSON.stringify(JSON.stringify(html)), { status: 200 });
+		const dom = new JSDOM(html);
+		const document = dom.window.document;
+		const title = document.querySelector("title");
+		console.log(title?.textContent);
+		return new Response(JSON.stringify(html), { status: 200 });
 	} catch (error: any) {
 		return new Response(JSON.stringify(error.message), { status: 500 });
 	}
--- app/components/MetaScrapper.tsx
+++ app/components/MetaScrapper.tsx
@@ -1,24 +1,22 @@
 import React, { useEffect, useState } from "react";
 
 export const MetaScrapper = () => {
-	const [url, setUrl] = useState("");
+  const [url, setUrl] = useState("");
 
-	useEffect(() => {
-		const getData = async () => {
-			const response = await fetch(`api/scrapper?url=${url}`, {
-				method: "GET",
-				headers: { "Content-Type": "application/json" },
-			})
-				.then((response) => response.json())
-				.then((data) => data.json());
-			console.log(response);
-		};
-		getData();
-	}, [url]);
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(`api/scrapper?url=${url}`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      }).then((response) => response.json());
+      console.log(response);
+    };
+    getData();
+  }, [url]);
 
-	return (
-		<div>
-			<input type="url" value={url} onChange={(e) => setUrl(e.target.value)} />
-		</div>
-	);
+  return (
+    <div>
+      <input type="url" value={url} onChange={(e) => setUrl(e.target.value)} />
+    </div>
+  );
 };
